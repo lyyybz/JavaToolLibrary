acea526548895154486b58a770b9d728b220186c
杨瑞敏 <159444616@qq.com>
2016-11-11 15:02:51 +0800

    Create maven project

---
+++ JavaToolLibrary/src/main/java/com/lyyybz/App.java
@@ -0,0 +1,13 @@
+package com.lyyybz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
