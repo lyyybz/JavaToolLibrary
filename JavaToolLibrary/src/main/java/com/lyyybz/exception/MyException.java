fc182509543e123905ccfb5f21b8b6934ef8493e
杨瑞敏 <159444616@qq.com>
2016-11-14 08:38:14 +0800

    Add my exception

---
+++ JavaToolLibrary/src/main/java/com/lyyybz/exception/MyException.java
@@ -0,0 +1,24 @@
+package com.lyyybz.exception;
+/**
+ * 
+ * @author lyyybz
+ *
+ */
+public class MyException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MyException()
+	{
+		super();
+	}
+	
+	public MyException(String message)
+	{
+		super(message);
+	}
+
+}
